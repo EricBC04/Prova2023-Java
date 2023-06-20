fbfb34c2f859e02ae4de6eeafcc6c5158a8eab04
Eric <eric.coraini@gmail.com>
2023-06-20 11:05:21 -0300

    Remover_Empresas

--- src/main/java/com/fatec/empresas/resources/EmpresasController.java
+++ src/main/java/com/fatec/empresas/resources/EmpresasController.java
@@ -1,11 +1,17 @@
 package com.fatec.empresas.resources;
 
 import java.net.URI;
+import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +27,29 @@ public class EmpresasController {
     
     @Autowired
     private EmpresasService empresasService;
+
+    @GetMapping("{id}")
+    public Empresas getEmpresas(@PathVariable int id){
+        return empresasService.getEmpresasById(id);
+    }
+
+     @GetMapping()
+    public List<Empresas> getEmpresas(){
+       return empresasService.getEmpresas();
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteempresas(@PathVariable int id){
+        empresasService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Void> editEmpresas(@PathVariable int id,@RequestBody Empresas empresas){
+        empresasService.update(id,empresas);
+        return ResponseEntity.ok().build();
+    }
+
 
     @PostMapping
     public ResponseEntity<Empresas> saveEmpresas(@RequestBody Empresas empresas){
